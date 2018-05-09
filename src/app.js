d23df00519cfa18b23eb90a89915be917de41795
comlewod <comlewod@163.com>
2018-05-09 14:37:40 +0800

    react router

--- src/app.js
+++ src/app.js
@@ -1,11 +1,32 @@
 'use strict'
 
 import react from 'react';
+import { render } from 'react-dom';
+import { Router, Route, Switch } from 'react-router';	
 import component from './components/component';
 import header from './components/header';
+//import { createBrowserHistory } from 'history'; 
+
+import { BrowserRouter } from 'react-router-dom';//browserHistroy模块已经从react-router独立出来，可以监听url变化，并生成location对象，并匹配到路由组件
 
 import './less/test';
-console.log('app test');
+import List from './list';
+
+//const history = createBrowserHistory();
 
 //react.render(`<component>`, document.getElementById('app'));
 
+//render(
+//	<Router history={history}>
+//		<Switch>
+//			<Route exact path="/" component={App} />
+//			<Route path="/test" component={Header} />
+//		</Switch>
+//	</Router>
+//, document.getElementById('app'));
+
+render(
+	<BrowserRouter>
+		<List />
+	</BrowserRouter>
+, document.getElementById('app'));
--- src/list.js
+++ src/list.js
@@ -1,5 +1,32 @@
-'use strict'
-import './less/list.less';
-import header from './components/header.jsx';
+import React, { Component } from 'react';
+import { Link, Route, Switch } from 'react-router-dom';
 
-console.log('list test');
+class List extends Component {
+	render() {
+		return (
+			<div>
+				<div>
+					<span><Link to="/">page</Link></span>
+					<span><Link to="/body">body</Link></span>
+				</div>
+				<Switch>
+					<Route exact path="/" component={Header} />
+					<Route path="/body" component={Body} />
+				</Switch>
+			</div>
+		);
+	}
+}
+
+const Header = () => (
+	<h2>HEADER</h2>
+);
+
+const Body = (props) => (
+	<div>
+		<h3>BODY</h3>
+		<Route path={`${props.path}/:name`} render={(a) => ( <div>haha</div> )} />
+	</div>
+);
+
+export default List;
